9cf48505ffa56b5991ed71cfdb73e37c348615ea
Gyumin Kim <160209091+gyum-k@users.noreply.github.com>
2025-12-04 20:21:08 +0900

    25.12.04

---
+++ js/common-v0.js
@@ -0,0 +1,42 @@
+/* 육사교 - 당직 관리 시스템 공통 JS */
+document.addEventListener("DOMContentLoaded", () => {
+
+  // 사이드바 메뉴 구현
+  fetch("../sidebar.json")
+    .then((res) => res.json())
+    .then((data) => {
+    const sidebarNav = document.querySelector("#site-sidebar #nav");
+
+      if (!sidebarNav) {
+        console.error("❌ #sidebar 요소를 찾을 수 없습니다.");
+        return;
+      }
+
+      const menuHTML = data.menu
+        .map((item) => {
+          return `
+          <div class="nav-item" data-key="${item.id}">
+            <img class="icon" src="${item.icon}" alt="${item.label} 아이콘" />
+            <div class="label">${item.label}</div>
+          </div>
+        `;
+        })
+        .join("");
+
+      sidebarNav.innerHTML = menuHTML;
+    })
+    .catch((error) => {
+      console.error("❌ sidebar.json 로딩 오류:", error);
+    });
+
+    
+    const btnToggle = document.getElementById('btnToggle');
+    const sidebar = document.getElementById('site-sidebar');
+    let collapsed = false;
+
+    btnToggle.addEventListener('click', ()=>{
+      collapsed = !collapsed;
+      if(collapsed) sidebar.classList.add('collapsed');
+      else sidebar.classList.remove('collapsed');
+    });
+});
