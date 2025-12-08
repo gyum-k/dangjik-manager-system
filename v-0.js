9af61fc92d1b3e76241a1aad4873c5f8107e7707
Gyumin Kim <160209091+gyum-k@users.noreply.github.com>
2025-12-08 19:54:49 +0900

    25.12.08

--- v-0.js
+++ v-0.js
@@ -1,13 +1,41 @@
 /* 육사교 - 당직 관리 시스템 공통 JS */
 document.addEventListener("DOMContentLoaded", () => {
 
-    // 사이드바 토글
-    const sidebarToggle = document.querySelector('.sidebar-toggle');
-    const sidebar = document.querySelector('#site-sidebar');
+  // 테마 초기화
+  function initTheme() {
+    const savedTheme = localStorage.getItem('dms.theme');
 
-    sidebarToggle.addEventListener('click', () => {
-      sidebar.classList.toggle('collapsed');
-    })
+    if (savedTheme) {
+      document.documentElement.setAttribute('data-theme', savedTheme);
+    } else {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      const theme = prefersDark ? 'dark' : 'light';
+      document.documentElement.setAttribute('data-theme', theme);
+      localStorage.setItem('dms.theme', theme);
+    }
+  }
+
+  initTheme()
+
+  // 테마 토글 구현
+  const themeToggle = document.querySelector('.theme-toggle');
+
+  themeToggle.addEventListener('click', () => {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute('data-theme') || 'light';
+    const theme = currentTheme === 'light' ? 'dark' : 'light';
+
+    html.setAttribute('data-theme', theme);
+    localStorage.setItem('dms.theme', theme);
+  })
+  
+  // 사이드바 토글
+  const sidebarToggle = document.querySelector('.sidebar-toggle');
+  const sidebar = document.querySelector('#site-sidebar');
+
+  sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+  })
 
   // 사이드바 메뉴
   fetch("./sidebar.json")
@@ -26,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <li data-key="${item.id}">
             <button class="nav-button" onclick="location.href='${item.link}'">
               <img class="icon" src="${item.icon}" alt="${item.label} 아이콘" />
-              <span>${item.label}</span>
+              <span class="title">${item.label}</span>
             </button
           </li>
         `;
